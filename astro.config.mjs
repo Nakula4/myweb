a8f8f1d2f36890abe0b668dc3bd78ad71ca5b16a
nakula4 <nahdlatusy28@gmail.com>
2026-08-13 05:35:32 +0700

    Siap deploy ke GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  // 1. TAMBAHKAN DUA BARIS INI UNTUK GITHUB PAGES
+  site: 'https://Nakula4.github.io', 
+  base: 'https://github.com/Nakula4/myweb.git', // SESUAIKAN dengan nama repositori GitHub Anda
+  
+  // Konfigurasi Tailwind Vite Anda (biarkan apa adanya)
   vite: {
     plugins: [tailwindcss()]
   }
