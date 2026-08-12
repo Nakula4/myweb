54cc5248f8f247b4f9bb772f1c88131cefbfc4f8
nakula4 <nahdlatusy28@gmail.com>
2026-08-13 05:53:45 +0700

    Ini Siap deploy ke GitHub Page

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   // 1. TAMBAHKAN DUA BARIS INI UNTUK GITHUB PAGES
   site: 'https://Nakula4.github.io', 
-  base: 'https://github.com/Nakula4/myweb.git', // SESUAIKAN dengan nama repositori GitHub Anda
+  base: '/Btools', // SESUAIKAN dengan nama repositori GitHub Anda
   
   // Konfigurasi Tailwind Vite Anda (biarkan apa adanya)
   vite: {
